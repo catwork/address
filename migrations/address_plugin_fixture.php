f8af0eb6f4ef805046da55e2281a165dbfc9c45e
José Ricardo <root@josericardo.eti.br>
2010-06-24 16:55:31 -0400

    Plugin Migrations

---
+++ migrations/address_plugin_fixture.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * AddressPluginFixture Migration
+ *
+ * @since 24/06/2010 16:48:32
+ */
+class AddressPluginFixture extends AppMigration {
+
+ var $estados = array(
+          'AC' => 'Acre',
+          'AL' => 'Alagoas',
+          'AP' => 'Amapá',
+          'AM' => 'Amazonas',
+          'BA' => 'Bahia',
+          'CE' => 'Ceará',
+          'DF' => 'Distrito Federal',
+          'ES' => 'Espírito Santo',
+          'GO' => 'Goiás',
+          'MA' => 'Maranhão',
+          'MT' => 'Mato Grosso',
+          'MS' => 'Mato Grosso do Sul',
+          'MG' => 'Minas Gerais',
+          'PA' => 'Pará',
+          'PB' => 'Paraíba',
+          'PE' => 'Pernambuco',
+          'PI' => 'Piauí',
+          'PR' => 'Paraná',
+          'RJ' => 'Rio de Janeiro',
+          'RN' => 'Rio Grande do Norte',
+          'RS' => 'Rio Grande do Sul',
+          'RO' => 'Rondônia',
+          'RR' => 'Roraima',
+          'SC' => 'Santa Catarina',
+          'SP' => 'São Paulo',
+          'SE' => 'Sergipe',
+          'TO' => 'Tocantins'
+  );
+
+  var $capitais = array(
+          'AC' => 'Rio Branco',
+          'AL' => 'Maceió',
+          'AP' => 'Macapá',
+          'AM' => 'Manaus',
+          'BA' => 'Salvador',
+          'CE' => 'Fortaleza',
+          'DF' => 'Brasília',
+          'ES' => 'Vitória',
+          'GO' => 'Goiânia',
+          'PA' => 'Belém',
+          'PB' => 'João Pessoa',
+          'PR' => 'Curitiba',
+          'PE' => 'Recife',
+          'PI' => 'Teresina',
+          'MA' => 'São Luís',
+          'MT' => 'Cuiabá',
+          'MS' => 'Campo Grande',
+          'MG' => 'Belo Horizonte',
+          'RJ' => 'Rio de Janeiro',
+          'RN' => 'Natal',
+          'RS' => 'Porto Alegre',
+          'RO' => 'Porto Velho',
+          'RR' => 'Boa Vista',
+          'SC' => 'Florianópolis',
+          'SP' => 'São Paulo',
+          'SE' => 'Aracaju',
+          'TO' => 'Palmas'
+  );
+
+  var $uses = array('Address.AddressCountry', 'Address.AddressState', 'Address.AddressCity', 'Address.AddressNeighborhood');
+
+/**
+ * Up Method
+ *
+ */
+	function up() {
+		$this -> {'Address.AddressCountry'} -> create();
+    $this -> {'Address.AddressCountry'} -> save(array('name' => 'Brasil', 'abbreviation' => 'BRA'));
+
+    $brasilId = $this -> {'Address.AddressCountry'} -> id;
+
+    foreach ($this -> estados as $abbrev => $name) {
+      $this -> {'Address.AddressState'} -> create();
+      $this -> {'Address.AddressState'} -> save(array(  'country_id' => $brasilId,
+                                                        'abbreviation' => $abbrev,
+                                                        'name' => $name
+                                               ));
+
+      $this -> {'Address.AddressCity'} -> create();
+
+      $this -> {'Address.AddressCity'} -> save(array(  'state_id' =>  $this -> {'Address.AddressState'} -> id,
+                                                       'name' => $this -> capitais[$abbrev]
+                                               ));
+
+      $this -> {'Address.AddressNeighborhood'} -> create();
+      $this -> {'Address.AddressNeighborhood'} -> save(array(  'city_id' =>  $this -> {'Address.AddressCity'} -> id,
+                                                               'name' => 'centro'
+                                               ));
+    }
+	}
+
+/**
+ * Down Method
+ *
+ */
+	function down() {
+	}
+}
+
+?>
---
+++ migrations/address_plugin_tables.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * AddressPluginTables Migration
+ *
+ * @since 24/06/2010 16:48:32
+ */
+class AddressPluginTables extends AppMigration {
+  var $tables = array(
+          'address_addresses' => array(
+                          'number' => array('type' => 'string', 'length' => 11),
+                          'more' => array('type' => 'string', 'length' => 255),
+                          'zipcode_id' => array('type' => 'integer', 'null' => false)
+          ),
+          'address_zipcodes' => array(
+                          'neighborhood_id' => array('type' => 'integer', 'null' => true),
+                          'city_id' => array('type' => 'integer', 'null' => false),
+                          'postal_code' => array('type' => 'string', 'null' => false, 'length' => 10),
+                          'street' => array('type' => 'string', 'length' => 128)
+          ),
+          'address_states' => array(
+                          'name' => array('type' => 'string', 'length' => 64, 'null' => false),
+                          'abbreviation' => array('type' => 'string', 'length' => 3),
+                          'country_id' => array('type' => 'integer', 'null' => false)
+          ),
+          'address_cities' => array(
+                          'name' => array('type' => 'string', 'length' => 128, 'null' => false),
+                          'state_id' => array('type' => 'integer', 'null' => false)
+          ),
+          'address_countries' => array(
+                          'name' => array('type' => 'string', 'length' => 128, 'null' => false),
+                          'abbreviation' => array('type' => 'string', 'length' => 3),
+          ),
+          'address_neighborhoods' => array(
+                          'name' => array('type' => 'string', 'length' => 128, 'null' => false),
+                          'city_id' => array('type' => 'integer', 'null' => false)
+          )
+  );
+
+  /**
+   * Up Method - Creating Tables
+   */
+  function up() {
+    foreach ($this -> tables as $name => $schema) {
+      $this -> createTable($name, $schema);
+    }
+  }
+
+  /**
+   * Down Method - Dropping Tables
+   */
+  function down() {
+    foreach ($this -> tables as $name => $schema) {
+      $this -> dropTable($name);
+    }
+  }
+}
+
+?>
