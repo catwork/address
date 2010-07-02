6cfd41c34d4851740b000e439d72ae3e202fdfb1
José Ricardo <root@josericardo.eti.br>
2010-07-02 16:09:27 -0400

    Adicionando nova behavior Addressable

---
+++ models/behaviors/addressable.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This behavior attaches the address to models which are not the main model being saved
+ *
+ * @author José Ricardo
+ */
+class AddressableBehavior extends ModelBehavior {
+
+  var $campos;
+
+	function beforeSave(&$model) {
+		$data =& $model->data[$model->name];
+
+    if ($model -> Address != null && !empty($data['Address'])) {
+      if (empty($data['Address']['zipcode_id'])) {
+        return false;
+      }
+      
+      $model -> Address -> create();
+      $model -> Address -> save($data['Address']);
+      $data['address_id'] =  $model -> Address -> id;
+    }
+  }
+}
+?>
