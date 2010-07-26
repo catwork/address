abc8580603a9790fef3477fc556b05573d9934ed
Zé Ricardo <contato@josericardo.eti.br>
2010-07-26 14:39:25 -0400

    Removendo a obrigatoriedade de zipcode_id em Address

--- models/address_address.php
+++ models/address_address.php
@@ -3,10 +3,6 @@ class AddressAddress extends AddressAppModel
 {
   var $name = 'AddressAddress';
   
-  public $validate = array(
-    'zipcode_id' => array('notEmpty')
-  );
-  
 	var $belongsTo = array('Zipcode' => array('className' => 'Address.AddressZipcode',  'foreignKey' => 'zipcode_id')) ;
 }
 ?>
